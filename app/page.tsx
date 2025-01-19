86bd7c3a4c4b4a8ccb71e372ab6ada39c7d31bef
screw-hand <screwhand0@gmail.com>
2025-01-19 14:29:53 +0800

    feat: init page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+"use client";
+
+import { MemeGenerator } from '@/components/meme-generator';
+import { ThemeProvider } from '@/components/theme-provider';
+
 export default function IndexPage() {
   return (
-    <div>
-      <h1>page1</h1>
-    </div>
-  )
+    <ThemeProvider>
+      <main className="container mx-auto px-4 py-8">
+        <h1 className="mb-8 text-center text-3xl font-bold">
+          梗图生成器
+        </h1>
+        <h3 className="mb-8 text-center text-xl font-bold">
+          无语💧 跟你讲不下去，典型的玩梗思维！！
+        </h3>
+        <MemeGenerator />
+      </main>
+    </ThemeProvider>
+  );
 }
---
+++ components/meme-generator.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { useState } from "react";
+import { Category } from "@/types/meme";
+
+export function MemeGenerator() {
+
+  return (
+    <div className="space-y-6">
+    </div>
+  )
+}
