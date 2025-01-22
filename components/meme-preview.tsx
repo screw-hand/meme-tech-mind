41ccc4f705dba8bd0188ca200545b179b22ed0ee
screw-hand <screwhand0@gmail.com>
2025-01-22 11:01:43 +0800

    feat: meme-preview

--- components/meme-preview.tsx
+++ components/meme-preview.tsx
@@ -1,9 +1,40 @@
+import { Icon } from "@iconify/react"
+
 import { MemeSettingsType } from "@/types/meme-settings"
+import ICON_LIST from "@/config/iconify-skill-icon-list"
 
 interface MemePreviewProps {
   settings: MemeSettingsType
 }
 
+const ICONIFY_TYPE_PREFIX = "skill-icons:"
+
 export function MemePreview({ settings }: MemePreviewProps) {
-  return <div>MemePreview</div>
+  const getIconifyIconName = (name: string) => {
+    let iconName = name
+    let result = `${ICONIFY_TYPE_PREFIX}${iconName}`
+    const filterIconifyList = ICON_LIST.filter((x) => x.includes(name))
+    if (filterIconifyList?.length > 0) {
+      iconName = filterIconifyList[0]
+    }
+    result = `${ICONIFY_TYPE_PREFIX}${iconName}`
+    console.log("getIconifyIconName", result)
+    return result
+  }
+
+  return (
+    <div className="meme-preview flex items-center justify-center">
+      <div className="relative mx-auto text-center">
+        <Icon
+          icon={getIconifyIconName(settings.source)}
+          className="mx-auto text-center text-9xl"
+        />
+        <span className="absolute right-1 top-[20px] text-5xl">💧</span>
+        <p>
+          无语,跟你讲不下去，
+          <br /> 典型的{settings.target}思维
+        </p>
+      </div>
+    </div>
+  )
 }
