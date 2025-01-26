e9cd97853c8a439f72fc68c01d0d6a9cf2e3600a
screw-hand <screwhand0@gmail.com>
2025-01-26 23:01:00 +0800

    ui: Wrap SettingBar label with shadcn Label component

--- components/setting-bar.tsx
+++ components/setting-bar.tsx
@@ -1,3 +1,5 @@
+import { Label } from "./ui/label"
+
 export function SettingBar({
   label,
   other,
@@ -9,13 +11,13 @@ export function SettingBar({
 }) {
   return (
     <div className="setting-bar">
-      <div className="item-center flex gap-2">
+      <Label className="item-center flex gap-2">
         <p className="flex-none py-[6px] text-sm font-medium">{label}</p>
         <div className="flex flex-auto flex-col">
           {children}
           {other && <div className="mt-2 text-xs text-gray-500">{other}</div>}
         </div>
-      </div>
+      </Label>
     </div>
   )
 }
